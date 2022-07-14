4e5b2d45d8909cc14714a526323f183e26808115
Tony Alvarez <tonyalva87@gmail.com>
2022-07-14 14:51:04 -0700

    add coin

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import './Coin.css'
+
+let coinImg;
 
 const Coin = (props) => {
-  return ()
+  const flip = Math.floor(Math.random() * 2)
+  let side;
+  if (flip == 0) {
+    side = 'heads'
+  } else if (flip == 1) {
+    side = 'tails'
+  }
+  coinImg = `./src/${side}.png`
+
+  return (
+    <Box>
+      <img className="CoinImg" src={coinImg} alt="{side}"/>
+    </Box>
+    )
 }
 
 export default Coin
